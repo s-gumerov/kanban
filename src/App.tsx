3636d7d5acaf02b5afa24f6e7355938120414d1b
Salavat <s.gumerov.work@gmail.com>
2023-05-05 11:42:11 +0500

    feat: реализован перехватчик ошибок error-boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import { withErrorBoundary } from 'react-error-boundary'
+import { ErrorFallback } from './services/errorFallback/ErrorFallback.tsx'
 
-export const App = () => {
-
-  return (
-      <h1>Kanban</h1>
-  )
+const App = () => {
+  return <h1>Kanban</h1>
 }
+export default withErrorBoundary(App, {
+  FallbackComponent: ErrorFallback,
+})
