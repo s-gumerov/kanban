d9b6b4cf656fddb424d89323159540748faaef39
Salavat <s.gumerov.work@gmail.com>
2023-05-09 11:08:17 +0500

    feat: реализована заглушка страницы NotFound

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,7 @@
+export const NotFound = (): JSX.Element => {
+  return (
+    <div className="not-found">
+      <h1 className="not-found__title title">NotFound</h1>
+    </div>
+  )
+}
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export * from './Auth'
 export * from './Main'
 export * from './Kanban'
+export * from './NotFound'
