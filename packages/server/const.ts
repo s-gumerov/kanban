f4bf6ff4b882c7bd6341defa3969ce3e8edd0a1c
s-gumerov <s.gumerov.work@gmail.com>
2023-05-13 22:55:01 +0500

    feat: добавлены зависимости в директории сервера

---
+++ packages/server/const.ts
@@ -0,0 +1,1 @@
+export const CLIENT_DIR = '../client/dist/client/'
---
+++ packages/server/index.ts
@@ -0,0 +1,9 @@
+import * as express from 'express'
+import * as path from 'path'
+import { CLIENT_DIR } from './const'
+
+const app = express()
+const { PORT = 3000 } = process.env
+
+app.use(express.static(path.join(__dirname, CLIENT_DIR)))
+app.listen(PORT)
