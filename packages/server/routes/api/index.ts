758a1b0a7cf710aa200efef39abdec0db62a4f5e
s-gumerov <s.gumerov.work@gmail.com>
2023-05-17 21:50:55 +0500

    chore: исправлена загрузка переменных окружения для docker-compose.yml и испавлен скрипт запуска сервера

--- packages/server/routes/api/index.ts
+++ packages/server/routes/api/index.ts
@@ -3,7 +3,7 @@ import { userRouter } from './user'
 import { API_URL } from '../../const'
 
 const router = Router()
-
+console.log('router started')
 router.use(API_URL, userRouter)
 
 export { router }
